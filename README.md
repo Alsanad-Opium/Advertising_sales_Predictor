d3f5c99b3a7d6b10b81f48c44d70b72953276f7f
Alsanad-Opium <alsanad112006@gmail.com>
2026-04-04 18:46:34 +0530

    created the /predict functiton in the app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advertising_sales_Predictor
 
 Here is the first commit after creating the model 
+
+the model was trained on kaggle here is the notebook link :- [kaggle link](https://www.kaggle.com/code/suspicious16/advertising-sales/edit)
--- app.py
+++ app.py
@@ -1,3 +1,38 @@
 import pickle 
+from flask import Flask,request,jsonify
+import numpy as np 
+import pandas as pd
 
 model = pickle.load(open('model/Advertising_model_sales_v1.pkl','rb'))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return " First ml and flask api bitch"
+    
+    
+@app.route('/predict', methods = ['POST'])
+
+def predict():
+
+    data  = request.get_json()
+    
+    features = data['features'] 
+     
+      
+    df_feat = pd.DataFrame(features, columns = ['TV Ad Budget ($)', 'Radio Ad Budget ($)', 'Newspaper Ad Budget ($)']) 
+    
+    pred = model.predict (df_feat)
+    return jsonify({
+        'prediction': pred.tolist()
+    })
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    app.run(debug = True)
