18e30fddafbc87cba1b3a7ef914b1cf20c6622df
Alsanad-Opium <alsanad112006@gmail.com>
2026-04-05 03:08:09 +0530

    added the health and the updated the predict function

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 model = pickle.load(open('model/Advertising_model_sales_v1.pkl','rb'))
 
-
+EXPECTED_FEATURES = 3
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,18 +20,39 @@ def predict():
 
     data  = request.get_json()
     
+     # 🔹 Check if data exists
+    if not data:
+            return jsonify({"error": "No input data provided"}), 400
+
+        # 🔹 Check key
+    if 'features' not in data:
+            return jsonify({"error": "'features' key missing"}), 400
+        
     features = data['features'] 
+    
+    # 🔹 Check type
+    if not isinstance(features, list):
+            return jsonify({"error": "Features must be a list"}), 400
+
+        # 🔹 Check length
+    if len(features) != EXPECTED_FEATURES:
+            return jsonify({
+                "error": f"Expected {EXPECTED_FEATURES} features"
+            }), 400
      
       
     df_feat = pd.DataFrame(features, columns = ['TV Ad Budget ($)', 'Radio Ad Budget ($)', 'Newspaper Ad Budget ($)']) 
     
     pred = model.predict (df_feat)
     return jsonify({
+        "status": "success",
         'prediction': pred.tolist()
     })
     
     
-    
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "running"})
     
     
 if __name__ == '__main__':
